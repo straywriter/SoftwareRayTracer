3fa4b05d55c7c8f39ab6edb35e28a47bb3e7efba
TA-MEI <2745982906@qq.com>
2020-12-22 23:53:08 +0800

    update

--- Demo/Base/6DemoAntialiasing.cpp
+++ Demo/Base/6DemoAntialiasing.cpp
@@ -1,29 +1,31 @@
 /**
  * @file 6DemoAntialiasing.cpp
- * 
+ *
  * @date 2020-12-17
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
-#include "Render.h"
-#include "Color.h"
 #include "Camera.h"
+#include "Color.h"
 #include "Hit.h"
+#include "Render.h"
 #include "Sphere.h"
 
-
-color ray_color(const Ray& r, const IHitable& world) {
+color ray_color(const Ray &r, const IHitable &world)
+{
     HitRecord rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+    if (world.hit(r, 0, infinity, rec))
+    {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     Vector3d unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main() {
+int main()
+{
 
     // Image
 
@@ -35,23 +37,27 @@ int main() {
     // World
 
     HittableList world;
-    world.add(make_shared<Sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<Sphere>(point3(0,-100.5,-1), 100));
+    world.add(make_shared<Sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<Sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     Carmera cam;
 
     // Render
 
+
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j)
+    {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
+        for (int i = 0; i < image_width; ++i)
+        {
             color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 Ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world);
             }
